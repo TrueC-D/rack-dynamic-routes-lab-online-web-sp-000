f524c7b2184d43e3d07136884dccf1e3e048789d
Alethea Cary <aletheacary@gmail.com>
2019-11-19 08:16:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,7 @@ class Application
       else
         resp.write "Item does not exist"
         resp.status = 400
-        ens
+      end
     else
       resp.write "Route not found"
       resp.status = 404
