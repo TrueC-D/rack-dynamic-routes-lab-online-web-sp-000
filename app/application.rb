90c553cffd9d71a4cb1dca6a0499cab391aaa38a
Alethea Cary <aletheacary@gmail.com>
2019-11-19 08:09:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,19 @@
+require_relative './item'
+
 class Application
 
   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
+    if req.path.match(/items/)
+      Item.all.include?(item_name)
 
+      resp.write item.price
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
 
 end
